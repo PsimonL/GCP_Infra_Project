30e954cbd81b86b3aaed95e14fb2aea4b06acd22
Szymon Rogowski <rogowski@student.agh.edu.pl>
2024-11-29 20:57:29 +0100

    more useful commands.sh

--- infra/commands.sh
+++ infra/commands.sh
@@ -4,8 +4,11 @@ tree /f
 go run main.go
 go mod init myapp
 
-docker build -t time-api .
-docker run -p 8080:8081 time-api
+docker build -t time-service .
+docker build --no-cache -t time-service .
+docker run -d -p 8081:8081 time-service # host port : container port
+docker run -it --entrypoint /bin/sh -p 8081:8081 time-service
+docker run -it -p 8081:8081 time-service
 
 curl http://localhost:8081/time
 
